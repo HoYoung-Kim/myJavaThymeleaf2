2343acd2f2567a33a8025e35e20e559e4a54a154
HoYoung-Kim <hykim001kr@gmail.com>
2021-10-20 15:13:18 +0900

    init

---
+++ src/main/java/com/example/youtubethymeleaf2/YoutubeThymeleaf2Application.java
@@ -0,0 +1,13 @@
+package com.example.youtubethymeleaf2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeThymeleaf2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeThymeleaf2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/youtubethymeleaf2/YoutubeThymeleaf2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youtubethymeleaf2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeThymeleaf2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
